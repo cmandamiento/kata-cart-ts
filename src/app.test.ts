946c5eb7acc7007886645cc09e613de8b736389e
luisgabriel.gomez <luisgabriel.gomez@pedidosya.com>
2021-10-05 12:47:17 -0300

    Iteration 1 update

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,4 +1,4 @@
-import Cart, { FruitDb, FruitType } from "./app";
+import Cart, { FruitDb, FruitType, PearPromotion, ApplePromotion } from "./app";
 
 describe("Cart test", () => {
   it("Cart allows add item", () => {
@@ -25,15 +25,86 @@ describe("Cart test", () => {
     items.set("Apple", 1);
 
     const newCart = new Cart(items);
-    expect(newCart.calculateTotal()).toBe(25);
+    expect(newCart.calculateTotal()).toBe(30);
   });
 
-  it("Cart calculates bulk prices", () => {
-    const Pear = FruitDb.Pear;
+  it("Pear promotion works, exact", () => {
+    const items = new Map();
+    items.set("Pear", 2);
 
+    const newCart = new Cart(items);
+    const newPearPromo = new PearPromotion();
+    expect(newPearPromo.calculateDiscount(newCart.items)).toBe(2);
+  });
+
+  it("Pear promotion works, exceeding", () => {
+    const items = new Map();
+    items.set("Pear", 3);
+
+    const newCart = new Cart(items);
+    const newPearPromo = new PearPromotion();
+    expect(newPearPromo.calculateDiscount(newCart.items)).toBe(2);
+  });
+
+  it("Apple promotion works, exact", () => {
+    const items = new Map();
+    items.set("Apple", 3);
+
+    const newCart = new Cart(items);
+    const newApplePromo = new ApplePromotion();
+    expect(newApplePromo.calculateDiscount(newCart.items)).toBe(10);
+  });
+
+  it("Apple promotion works, exceeding", () => {
+    const items = new Map();
+    items.set("Apple", 5);
+
+    const newCart = new Cart(items);
+    const newApplePromo = new ApplePromotion();
+    expect(newApplePromo.calculateDiscount(newCart.items)).toBe(10);
+  });
+
+
+  it("Cart calculates bulk prices on single fruit add", () => {
+    const Pear = FruitDb.Pear;
+    const Apple = FruitDb.Apple;
+  
     const newCart = new Cart();
     newCart.add(Pear, 2);
+    newCart.add(Apple, 3);
 
-    expect(newCart.calculateTotal()).toBe(18);
+    expect(newCart.calculateTotal()).toBe(68);
+  });
+
+  it("Cart calculates bulk priceson multiple fruit add", () => {
+    const Pear = FruitDb.Pear;
+    const Apple = FruitDb.Apple;
+  
+    const newCart = new Cart();
+    newCart.add(Pear, 1);
+    newCart.add(Apple, 1);
+    newCart.add(Pear, 1);
+    newCart.add(Apple, 1);
+    newCart.add(Apple, 1);
+    
+    expect(newCart.calculateTotal()).toBe(68);
+  });
+
+  it("Cart calculates bulk prices with bulk excess", () => {
+    const Pear = FruitDb.Pear;
+ 
+    const newCart = new Cart();
+    newCart.add(Pear, 3);
+
+    expect(newCart.calculateTotal()).toBe(28);
+  });
+
+  it("Cart calculates bulk prices with 10 bananas", () => {
+    const Banana = FruitDb.Banana;
+
+    const newCart = new Cart();
+    newCart.add(Banana, 10);
+
+    expect(newCart.calculateTotal()).toBe(150);
   });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
+type Sku = string;
+
 export interface FruitType {
-  sku: string;
+  sku: Sku;
   name: string;
   price: number;
 }
@@ -18,7 +20,7 @@ export const FruitDb = {
   Apple: {
     sku: "Apple",
     name: "Apple",
-    price: 15,
+    price: 20,
   },
   Orange: {
     sku: "Orange",
@@ -27,10 +29,38 @@ export const FruitDb = {
   },
 };
 
-type CartType = Map<string, number>;
+type CartType = Map<Sku, number>;
 
 export interface PromoSpecification {
-  isSatisfiedBy: (cart: CartType) => void;
+  calculateDiscount: (cart: CartType) => number;
+}
+
+export class PearPromotion implements PromoSpecification {
+  calculateDiscount(cart: CartType): number {
+    let pearCount = 0;
+
+    for (const [key, value] of cart) {
+      if (key === "Pear") {
+        pearCount += value;
+      }
+    }
+
+    return 2 * Math.floor(pearCount / 2);
+  }
+}
+
+export class ApplePromotion implements PromoSpecification {
+  calculateDiscount(cart: CartType): number {
+    let appleCount = 0;
+
+    for (const [key, value] of cart) {
+      if (key === "Apple") {
+        appleCount += value;
+      }
+    }
+
+    return 10 * Math.floor(appleCount / 3);
+  }
 }
 
 // Pears => 40
@@ -38,10 +68,14 @@ export interface PromoSpecification {
 // Pear promo => -4
 // Pear & Apple promo => -10
 
-// const promoSpecfication = (cart: CartType) => {};
+const promoSpecfication = (cart: CartType) => { };
 
 class Cart {
   items: CartType;
+  discounts: Array<PromoSpecification> = [
+    new PearPromotion(),
+    new ApplePromotion(),
+  ];
 
   constructor(items: CartType = new Map()) {
     this.items = items;
@@ -61,13 +95,18 @@ class Cart {
 
   calculateTotal(): number {
     let total = 0;
+    let discountTotal = 0;
 
     // Map to Array [[key, value], [key, value]]
     Array.from(this.items.entries()).forEach(([key, value]) => {
       total = FruitDb[key].price * value + total;
     });
 
-    return total;
+    Array.from(this.discounts.entries()).forEach(([key, value]) => {
+      discountTotal += value.calculateDiscount(this.items);
+    });
+
+    return total - discountTotal;
   }
 }
 
